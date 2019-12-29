040c44e350529e846678910ec42ffa53dadb27de
Hồ Phan Minh <38271639+Akashi195@users.noreply.github.com>
2019-12-30 00:45:05 +0700

    restrict Admin
    restrict Admin

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -1,5 +1,7 @@
+const restrictAdmin = require('../middlewares/UserOnly.mdw');
+
 module.exports = function(app){
     app.use('/',require('../routes/routes'));
     app.use('/player',require('../routes/player.routes'));
-    app.use('/admin',require('../routes/admin.routes'));
+    app.use('/admin',restrictAdmin,require('../routes/admin.routes'));
 }
